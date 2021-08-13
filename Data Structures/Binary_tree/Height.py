96bda99c85c7814907cabcce3367d5c6c41f30f3
Abhishek19009 <heddyrogue@gmail.com>
2021-08-13 09:37:41 +0530

    BinarySearchTree addition

--- Data Structures/Binary_tree/Height.py
+++ Data Structures/Binary_tree/Height.py
@@ -4,3 +4,13 @@ between that node and leaf of tree.
 
 Height to tree is height of its root node.
 '''
+
+
+def height(self, node=None):
+    if node is None:
+        return -1
+
+    height_left = self.height(node.left)
+    height_right = self.height(node.right)
+
+    return 1 + max(height_right, height_left)
